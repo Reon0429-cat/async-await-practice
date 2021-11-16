33ba9a7c323a370fdf39845986479d451420638b
Reon0429-cat <oonishireon110@gmail.com>
2021-11-16 12:33:05 +0900

    複数エラーハンドリング

--- async-await-practice/ViewController.swift
+++ async-await-practice/ViewController.swift
@@ -9,14 +9,23 @@ import UIKit
 
 struct User: Decodable {
     let name: String
+    let iconURL: URL
 }
 
 enum SomeError: Error {
     case invalidURL(url: URL)
 }
 
-class ViewController: UIViewController {
+extension Data {
+    
+    func get() throws -> Data {
+        return Data()
+    }
+    
+}
 
+class ViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -24,10 +33,39 @@ class ViewController: UIViewController {
         
     }
     
+    func fetchUserIcon(for id: Int) async throws -> Data {
+        let url = URL(string: "https://koherent.org/fake-service/api/user?id=\(id)")!
+        let data = try await downloadData2(from: url)
+        let user = try JSONDecoder().decode(User.self, from: data)
+        let icon = try await downloadData2(from: user.iconURL)
+        return icon
+    }
+    
+    func fetchUserIcon(for id: Int,
+                       completion: @escaping (Result<Data, Error>) -> Void) {
+        let url = URL(string: "https://koherent.org/fake-service/api/user?id=\(id)")!
+        downloadData(from: url) { data in
+            do {
+                let data = try data.get()
+                let user = try JSONDecoder().decode(User.self, from: data)
+                self.downloadData(from: user.iconURL) { icon in
+                    do {
+                        let icon = try icon.get()
+                        completion(.success(icon))
+                    } catch {
+                        completion(.failure(error))
+                    }
+                }
+            } catch {
+                completion(.failure(error))
+            }
+        }
+    }
+    
     func fetchUser2(for id: Int) async throws -> User {
         do {
             let data = try await downloadData2(from: URL(string: ""))
-            let user = try! JSONDecoder().decode(User.self, from: data)
+            let user = try JSONDecoder().decode(User.self, from: data)
             return user
         } catch {
             throw error

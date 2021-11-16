83634338250eef24979fd21cbcb5f36b5cdbf9f3
Reon0429-cat <oonishireon110@gmail.com>
2021-11-16 11:57:35 +0900

    練習

--- async-await-practice/ViewController.swift
+++ async-await-practice/ViewController.swift
@@ -7,13 +7,43 @@
 
 import UIKit
 
+struct User: Decodable {
+    let name: String
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        
     }
-
-
+    
+    func fetchUser(for id: Int) async -> User {
+        let url = URL(string: "https://koherent.org/fake-service/api/user?id=\(id)")!
+        let data = await downloadData(from: url)
+        let user = try! JSONDecoder().decode(User.self, from: data)
+        return user
+    }
+    
+    func downloadData(from url: URL) async -> Data {
+        return Data()
+    }
+    
+    func fetchUser(for id: Int,
+                   completion: @escaping (User) -> Void) {
+        let url = URL(string: "https://koherent.org/fake-service/api/user?id=\(id)")!
+        downloadData(from: url) { data in
+            let user = try! JSONDecoder().decode(User.self, from: data)
+            completion(user)
+        }
+    }
+    
+    func downloadData(from url: URL,
+                      completion: @escaping (Data) -> Void) {
+        completion(Data())
+    }
+    
 }
 
